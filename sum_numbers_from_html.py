9803fbcbaab13b47db9f9b690ca5c46b2c9bf868
Roy Contreras Benitez <roy7474@gmail.com>
2023-09-18 23:41:26 -0400

    list containing all of the string_int have been created and displays all of the numbers. However, the sum is not equivalent to the desired amount. Need to copy only the numbers that are next to comments on the pages source

--- sum_numbers_from_html.py
+++ sum_numbers_from_html.py
@@ -9,7 +9,8 @@ from bs4 import BeautifulSoup
 import ssl
 import re
 
-numbers = 0
+numbers = []
+count=0
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
@@ -20,25 +21,33 @@ html = urllib.request.urlopen(url, context=ctx).read() #urlopen(url, context=ctx
 soup = BeautifulSoup(html, "html.parser")
 
 # Retrieve all of the anchor tags
-tags = soup('p')
-for tag in tags:
-    numbers += 1
+#tags = soup('')
+#for tag in tags:
+#    numbers += 1
     # Look at the parts of a tag
-    print('TAG:', tag)
-    print('URL:', tag.get('href', None))
-    print('Contents:', tag.contents[0])
-    print('Attrs:', tag.attrs)
-print(numbers)
+#    print('TAG:', tag)
+#    print('URL:', tag.get('href', None))
+#    print('Contents:', tag.contents[0])
+#    print('Attrs:', tag.attrs)
+#print(numbers)
+text = str(soup)
+numbers_str = re.findall(r'\d+',text)
+numbers = [int(num_str) for num_str in numbers_str]
+total =  sum(numbers)
+print(total)
+print(numbers_str)
 
 
+
+
+'''
 for line in tags:
     text = line.get_text()
-    numbers.extend(re.findall(r'\d+', text))
+    count.extend(re.findall(r'\d+', text))
     #Extract the number from each of the lines using a regular expression and the findall() method
-#    x = re.findall('([0-9]+)', words)
-#    if len(x) > 0:                          #tracting only strings
-#        for val in x:
-#            val = int(val)                  #converting strings into intergers
-#            count.append(val)   
-
-print(numbers)
+    x = re.findall('([0-9]+)', text)
+    if len(x) > 0:                          #tracting only strings
+        for val in x:
+            val = int(val)                  #converting strings into intergers
+            count.append(val)   
+'''
